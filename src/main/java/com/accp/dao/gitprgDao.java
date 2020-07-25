2a99d28ba6f0a692a4a082a2000f4115bc87a370
Mingbase <1273364142@qq.com>
2020-07-25 09:27:04 +0800

    第二次提交git

--- src/main/java/com/accp/dao/gitprgDao.java
+++ src/main/java/com/accp/dao/gitprgDao.java
@@ -5,5 +5,7 @@ public class gitprgDao {
 	private void syso() {
 		System.out.println("第一次通过git修改dao");
 		System.out.println("第二次通过git修改dao");
+		System.out.println("使用A分支修改dao");
+		System.out.println("第二次提交git项目");
 	}
 }
